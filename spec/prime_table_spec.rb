0166b46738464a4c1d448b3edd09e915dd60eaa2
Luis De Castro <luisdecastro510@gmail.com>
2016-12-22 17:03:09 -0800

    add spec for making primes

--- spec/prime_table_spec.rb
+++ spec/prime_table_spec.rb
@@ -2,6 +2,7 @@ require_relative "../prime_table"
 
 describe PrimeTable do
   let(:table) {PrimeTable.new(5)}
+  let(:primes) {[2,3,5,7,11]}
 
   describe "#is_prime?" do
     it "returns true if a number is prime" do
@@ -9,6 +10,15 @@ describe PrimeTable do
     end
     it "returns false if a number is not prime" do
       expect(table.is_prime?(15)).to be(false)
+    end
+  end
+
+  describe "#make_primes" do
+    it "returns an array of primes" do
+      expect(table.make_primes).to eq(primes)
+    end
+    it "returns an array the same size as input size" do
+      expect(table.make_primes.size).to eq(table.size)
     end
   end
 end
